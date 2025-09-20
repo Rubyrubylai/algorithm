d9686096fc7b97300c3b8706e32daf0913c99a93
Rubyrubylai <r844312@gmail.com>
2025-09-20 10:39:06 +0200

    feat: add kadane

---
+++ array/kadane.py
@@ -0,0 +1,22 @@
+# find the maximum subarray sum
+def maxSubarraySum(arr):
+
+  res = arr[0]
+  maxEnding = arr[0]
+
+  for i in range(1, len(arr)):
+
+    # Find the maximum sum ending at index i by either extending
+    # the maximum sum subarray ending at index i - 1 or by
+    # starting a new subarray from index i（reset sum to 0）
+    maxEnding = max(maxEnding + arr[i], arr[i])
+
+    # Update res if maximum subarray sum ending at index i > res
+    res = max(res, maxEnding)
+    print(res, maxEnding)
+
+  return res
+
+
+arr = [2, 3, -8, 7, -1, 2, 3]
+print(maxSubarraySum(arr))
